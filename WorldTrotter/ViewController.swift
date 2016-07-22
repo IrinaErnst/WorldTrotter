c5e56083290698ecf5aec5b7be3149b05bf31c44
SunFloweri <irinakalashnikova@gmail.com>
2016-07-22 08:50:31 -0400

    Initial commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Irina Kalashnikova on 7/22/16.
+//  Copyright © 2016 Irina Kalashnikova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
